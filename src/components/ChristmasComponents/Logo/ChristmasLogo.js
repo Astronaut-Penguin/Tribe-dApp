036d1367c493c63f1aad3756129e9fc60c30d271
Alejo Viola <viola.alejo@outlook.com>
2021-12-19 19:31:42 -0300

    Adding style support on ChristmasLogo.js

--- src/components/ChristmasComponents/Logo/ChristmasLogo.js
+++ src/components/ChristmasComponents/Logo/ChristmasLogo.js
@@ -7,15 +7,15 @@ import styles from './ChristmasLogo.module.css';
 // Logo Import
 import ChristmasLogoHat from '../../../images/ChristmasLogo.png';
 
-// style: CSSProperties - En la etiqueta <button> se debe poner en la propiedad Style una variable 
+// style: CSSProperties - En la etiqueta <button> se debe poner en la propiedad Style una variable
 //                          para enviarle CSS desde fuera cuando es llamado en otro componente.
 
-const ChristmasLogo = () => {
-    return (
-        <a href="/">
+const ChristmasLogo = ({ style }) => {
+	return (
+		<a href="/" style={style}>
 			<img src={ChristmasLogoHat} alt="logo" className={styles.TribeLogo} />
 		</a>
-    );
+	);
 };
 
 export default ChristmasLogo;
