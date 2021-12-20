42bed9769b999a188183c315cb879afe3e88299d
FacundoRago <facundorago98@gmail.com>
2021-12-20 12:34:14 -0300

    Updated some Christmas Colours
    Updated some buttons and labels with Christmas Red.

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -7,7 +7,7 @@ import styles from './Button.module.css';
 // BUY BUTTON COMPONENT
 // text: STRING - Texto que va en el boton.
 // onClick: Function - Funcion que es llamada cuando se hace click en el boton.
-// style: CSSProperties - En la etiqueta <button> se debe poner en la propiedad Style una variable 
+// style: CSSProperties - En la etiqueta <button> se debe poner en la propiedad Style una variable
 //                          para enviarle CSS desde fuera cuando es llamado en otro componente.
 
 const BuyButton = ({ onClick, text }) => {
