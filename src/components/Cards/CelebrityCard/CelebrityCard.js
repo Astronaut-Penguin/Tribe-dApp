db8e563f3511307db842c1bf8133f1b64d126dcb
Alejo Viola <viola.alejo@outlook.com>
2021-12-21 11:37:32 -0300

    Adding Button color state support on CelebrityCard

--- src/components/Cards/CelebrityCard/CelebrityCard.js
+++ src/components/Cards/CelebrityCard/CelebrityCard.js
@@ -7,34 +7,37 @@ import styles from './CelebrityCard.module.css';
 //- Button Imports
 import LiveButton from '../../Button/Button';
 
-const CelebrityCard = () => {
-    
-    const image = './assets/nft/soon.png';
+const CelebrityCard = ({ completed }) => {
+	const image = './assets/nft/soon.png';
 
-    return (
-        <div className={styles.CelebrityNftCard}>
+	return (
+		<div className={styles.CelebrityNftCard}>
 			{image && (
 				<div
 					className={styles.CelebrityNftImage}
 					style={{ backgroundImage: 'url(' + image + ')' }}
-				>   
-                    <LiveButton text="Live" onClick='/' />
+				>
+					<LiveButton
+						text="Live"
+						onClick="/"
+						grey={completed ? true : false}
+						green={!completed ? true : false}
+					/>
 				</div>
 			)}
 
 			<div className={styles.CelebrityNftDataContainer}>
-                <div 
-                    className={styles.CelebrityPicture}
-                    style={{ backgroundImage: 'url(' + image + ')' }}
-                >
-                </div>
+				<div
+					className={styles.CelebrityPicture}
+					style={{ backgroundImage: 'url(' + image + ')' }}
+				></div>
 				<div className={styles.CelebrityNftData}>
 					<h2>Nombre Apellido</h2>
 					<h3>@RedSocial</h3>
 				</div>
 			</div>
 		</div>
-    );
+	);
 };
 
 export default CelebrityCard;
