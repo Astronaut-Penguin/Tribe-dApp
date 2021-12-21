3f06d10b337dec18b039f3ab0af05d755496fc1e
Alejo Viola <viola.alejo@outlook.com>
2021-12-21 11:36:59 -0300

    Adding color state support on Button

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -7,15 +7,27 @@ import styles from './Button.module.css';
 // BUY BUTTON COMPONENT
 // text: STRING - Texto que va en el boton.
 // onClick: Function - Funcion que es llamada cuando se hace click en el boton.
-// style: CSSProperties - En la etiqueta <button> se debe poner en la propiedad Style una variable 
+// style: CSSProperties - En la etiqueta <button> se debe poner en la propiedad Style una variable
 //                          para enviarle CSS desde fuera cuando es llamado en otro componente.
 
-const BuyButton = ({ onClick, text, green }) => {
-    return (
-        <button className={styles.CelebrityNftBuy} onClick={onClick}>
-			<p>{ text }</p>
+//STYLES COLORS
+//NO PASAR ESTOS VALORES AL MISMO TIEMPO
+//SOLO ELEGIR UNO
+// - green: true - Boton verde.
+// - grey: true - Boton con fondo gris.
+
+const BuyButton = ({ onClick, text, style, green, grey }) => {
+	return (
+		<button
+			className={`${styles.CelebrityNftBuy} 
+            ${green ? styles.Green : styles.Gradient}
+            ${grey ? styles.Grey : styles.Gradient}`}
+			onClick={onClick}
+			style={style}
+		>
+			<p>{text}</p>
 		</button>
-    );
+	);
 };
 
 export default BuyButton;
