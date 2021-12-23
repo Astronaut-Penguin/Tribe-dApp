eddf786d8deacc61773993f151f4046ec7f429b3
Alejo Viola <viola.alejo@outlook.com>
2021-12-23 00:12:43 -0300

    Adding automatic cards creator support

--- src/components/Celebrity/Celebrity.js
+++ src/components/Celebrity/Celebrity.js
@@ -3,39 +3,41 @@
 ///////////////////////
 
 //- React
-import React from 'react';
+import React, { useState } from 'react';
 
 //- Style
-import styles from './CelebrityMenu.module.css';
+import styles from './Celebrity.module.css';
 
+//- Views
+import CelebrityMenu from '../CelebrityMenu/CelebrityMenu';
+import CelebrityView from '../CelebrityView/CelebrityView';
 //- Components
-import ChristmasBanner from '../ChristmasComponents/Banners/ChristmasBanner';
 import CelebrityCard from '../Cards/CelebrityCard/CelebrityCard';
 
-const CelebrityMenu = () => {
+//-DATA
+import Artists from '../../data/artist.json';
+
+const Celebrity = () => {
+	//- View Rendered
+	const [view, setView] = useState(0);
+
+	//8888888888888888888888888888888888888888888888888888888//
+
+	const cards = Artists.data.map(function (value, i, a) {
+		return <CelebrityCard />;
+	});
+
+	//8888888888888888888888888888888888888888888888888888888//
+
+	//////////////
+	//  RENDER  //
+	//////////////
 	return (
 		<>
-			<ChristmasBanner
-				title={'Celebrity & Brands TRIBEPOP NFTs'}
-				text={
-					'Each TRIBEPOP is a re-creation of a celebrity or brand and these will be characters ready to use in our next role-playing game in our metaverse!'
-				}
-				text2={
-					'You can only buy them at this value in this unique opportunity, then you can only get one if someone wants to sell'
-				}
-				characters
-			/>
-
-			<section className={styles.Container}>
-				<div className={styles.Section}>
-					<h2 className={styles.Title}>Live Sales</h2>
-					<div className={styles.CardContainer}>
-						<CelebrityCard />
-					</div>
-				</div>
-			</section>
+			{view == 0 && <CelebrityMenu cards={cards} />}
+			{view == 1 && <CelebrityView />}
 		</>
 	);
 };
 
-export default CelebrityMenu;
+export default Celebrity;
