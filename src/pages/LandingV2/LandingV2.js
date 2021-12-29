e47a4db7e99f5416ca440bae4ece5b5012c4ce2c
Alejo Viola <viola.alejo@outlook.com>
2021-12-29 09:43:18 -0300

    Preparing landing v2 structure

---
+++ src/pages/LandingV2/LandingV2.js
@@ -0,0 +1,11 @@
+//- React Imports
+import React from 'react';
+
+//- Style Imports
+import styles from './LandingV2.module.css';
+
+const LandingV2 = () => {
+	return <></>;
+};
+
+export default LandingV2;
