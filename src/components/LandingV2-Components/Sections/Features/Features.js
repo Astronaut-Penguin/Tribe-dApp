6d8b276a33dcbbe92f6a91e2981a7383bd2349c1
FedeMar13 <federicojaviermarquez@hotmail.com>
2021-12-29 18:30:51 -0300

    add table

--- src/components/LandingV2-Components/Sections/Features/Features.js
+++ src/components/LandingV2-Components/Sections/Features/Features.js
@@ -13,38 +13,38 @@ import LaunchpadAllocations from '../../../../images/LaunchpadAllocations.png';
 import GamificationRigging from '../../../../images/GamificationRigging.png';
 
 const FeatureSection = () => {
-    return (
-        <section className={styles.Container}>
-            <h2>Features</h2>
-            <h3>Our NTFs attributes</h3>
-            <div className={styles.Cards}>
-                <div className={styles.Card}>
-                    <img src={CelebrityValidated}/>
-                    <span>Celebrity Validated</span>
-                </div>
-                <div className={styles.Card}>
-                    <img src={ScheduledBurns}/>
-                    <span>Scheduled Burns</span>
-                </div>
-                <div className={styles.Card}>
-                    <img src={MetaverseSkinReady}/>
-                    <span>Metaverse Skin Ready</span>
-                </div>
-                <div className={styles.Card}>
-                    <img src={StakingRewards}/>    
-                    <span>Staking Rewards</span>
-                </div>
-                <div className={styles.Card}>
-                    <img src={LaunchpadAllocations}/>
-                    <span>Launchpad Allocations</span>
-                </div>
-                <div className={styles.Card}>
-                    <img src={GamificationRigging}/>
-                    <span>Gamification Rigging</span>
-                </div>
-            </div>
-        </section>
-    );
+	return (
+		<section className={styles.Container}>
+			<h2>Features</h2>
+			<h3>Our NTFs attributes</h3>
+			<div className={styles.Cards}>
+				<div className={styles.Card}>
+					<img src={CelebrityValidated} />
+					<span>Celebrity Validated</span>
+				</div>
+				<div className={styles.Card}>
+					<img src={ScheduledBurns} />
+					<span>Scheduled Burns</span>
+				</div>
+				<div className={styles.Card}>
+					<img src={MetaverseSkinReady} />
+					<span>Metaverse Skin Ready</span>
+				</div>
+				<div className={styles.Card}>
+					<img src={StakingRewards} />
+					<span>Staking Rewards</span>
+				</div>
+				<div className={styles.Card}>
+					<img src={LaunchpadAllocations} />
+					<span>Launchpad Allocations</span>
+				</div>
+				<div className={styles.Card}>
+					<img src={GamificationRigging} />
+					<span>Gamification Rigging</span>
+				</div>
+			</div>
+		</section>
+	);
 };
 
 export default FeatureSection;
