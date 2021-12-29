9116af6841ebc219a8fa73e2af90765676fe95bb
FacundoRago <facundorago98@gmail.com>
2021-12-29 17:43:04 -0300

    No major changes

--- src/components/LandingV2-Components/Sections/Features/Features.js
+++ src/components/LandingV2-Components/Sections/Features/Features.js
@@ -2,5 +2,12 @@ import React from 'react';
 import styles from './Features.module.css';
 
 const FeatureSection = () => {
-    
-}
+    return (
+        <section className={styles.Container}>
+                <h2>Features</h2>
+                <h3>Our NTFs attributes</h3>
+        </section>
+    );
+};
+
+export default FeatureSection;
