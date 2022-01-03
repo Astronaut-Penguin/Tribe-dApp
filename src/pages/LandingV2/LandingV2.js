6def6703777926dbaa9770cc0b0186017df46756
FacundoRago <facundorago98@gmail.com>
2022-01-03 15:33:39 -0300

    New FlatButton added
    Yet to be fixed

--- src/pages/LandingV2/LandingV2.js
+++ src/pages/LandingV2/LandingV2.js
@@ -6,11 +6,15 @@ import styles from './LandingV2.module.css';
 
 //- Features Section Import
 import FeatureSection from '../../components/LandingV2-Components/Sections/Features/Features';
+import FlatButton from '../../components/LandingV2-Components/FlatButton/FlatButton';
 
 const LandingV2 = () => {
 	return (
 		<>
 			<FeatureSection />
+			<FlatButton onClick={'/'} text={'Este es el Big'}/>
+			<FlatButton onClick={'/'} text={'Este es el Mid'} mid/>
+			<FlatButton onClick={'/'} text={'Este es el Small'} small/>
 		</>
 	);
 };
