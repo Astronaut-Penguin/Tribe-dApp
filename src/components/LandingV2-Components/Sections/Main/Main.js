250192fe8f218d7b75dad96e5c9df38de5d2561a
Alejo Viola <viola.alejo@outlook.com>
2022-01-03 12:35:51 -0300

    Fixing Main.js & Main.module.css

--- src/components/LandingV2-Components/Sections/Main/Main.js
+++ src/components/LandingV2-Components/Sections/Main/Main.js
@@ -2,39 +2,54 @@
 import React from 'react';
 
 //- Import Styles
-import styles from './Main.module.css'
+import styles from './Main.module.css';
 
 //- Import Images
-import PlebCharacter3 from './assets/PlebCharacter3.png'
-import Binance from './assets/Binance.png'
-import Telegram from './assets/Telegram.png'
-import Twitter from './assets/Twitter.png'
-import Pancake from './assets/Pankake.png'
-import Bsc from './assets/bsc-8 2.png'
+import PlebCharacter3 from './assets/PlebCharacter3.png';
+import Binance from './assets/Binance.png';
+import Telegram from './assets/Telegram.png';
+import Twitter from './assets/Twitter.png';
+import Pancake from './assets/Pankake.png';
+import Bsc from './assets/bsc-8 2.png';
 
 const MainSection = () => {
-    return (
-        <section className={styles.Container}>
-            <div className={styles.Celebrities}>
-                Celebrities <span>TribePop</span> on the Metaverse
-            </div>
-            
-            <p>We launch NFTs. We build the future.</p>
-            <img src={PlebCharacter3} />
+	return (
+		<section className={styles.Container}>
+			<div className={styles.Celebrities}>
+				Celebrities <span>TribePop</span> on the Metaverse
+			</div>
 
-            <p>Buy <b>TRIBE Tokens</b> on</p>
-            <button>Pankake SWAP</button>
-            <p>Exclusively on <img src={Binance}/> <b>Binance Smart Chain</b></p><br />
+			<p className={styles.Text}>
+				We launch NFTs. <br />
+				We build the future.
+			</p>
+			<img src={PlebCharacter3} />
 
-            <div className={styles.Images}>
-                <a title='Telegram' href='/'><img src={Telegram} alt='Telegram'/></a>
-                <a title='Twitter' href='/'><img src={Twitter} alt='Twitter'/></a>
-                <a title='Pancake' href='/'><img src={Pancake} alt='Pancake'/></a>
-                <a title='Pancake' href='/'><img src={Bsc} alt='Pancake'/></a>
-            </div>
+			<p className={styles.Text}>
+				Buy <b>TRIBE Tokens</b> on
+			</p>
+			<button>Pankake SWAP</button>
+			<p className={styles.Text}>
+				Exclusively on <img src={Binance} /> <b>Binance Smart Chain</b>
+			</p>
+			<br />
 
-        </section>
-    );
+			<div className={styles.Images}>
+				<a title="Telegram" href="/" className={styles.Social}>
+					<img src={Telegram} alt="Telegram" />
+				</a>
+				<a title="Twitter" href="/" className={styles.Social}>
+					<img src={Twitter} alt="Twitter" />
+				</a>
+				<a title="Pancake" href="/" className={styles.Social}>
+					<img src={Pancake} alt="Pancake" />
+				</a>
+				<a title="BSCScan" href="/" className={styles.Social}>
+					<img src={Bsc} alt="BSCScan" />
+				</a>
+			</div>
+		</section>
+	);
 };
 
 export default MainSection;
