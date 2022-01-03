b3565a05b8d6db97883ba09db31a16dc8b58320d
FacundoRago <facundorago98@gmail.com>
2022-01-03 14:06:07 -0300

    Added Large and Medium Phone Support

--- src/components/LandingV2-Components/Sections/Main/Main.js
+++ src/components/LandingV2-Components/Sections/Main/Main.js
@@ -11,6 +11,7 @@ import Telegram from './assets/Telegram.png';
 import Twitter from './assets/Twitter.png';
 import Pancake from './assets/Pankake.png';
 import Bsc from './assets/bsc-8 2.png';
+import PanButton from './assets/PanButton.png'
 
 const MainSection = () => {
 	return (
@@ -23,13 +24,17 @@ const MainSection = () => {
 				We launch NFTs. <br />
 				We build the future.
 			</p>
-			<img src={PlebCharacter3} />
+			<img className={styles.Characters} src={PlebCharacter3} />
 
 			<p className={styles.Text}>
 				Buy <b>TRIBE Tokens</b> on
-			</p>
-			<button>Pankake SWAP</button>
-			<p className={styles.Text}>
+			</p><br />
+			
+            <button className={styles.PancakeButton}> 
+                <img src={PanButton} /> <b>PankakeSwap</b>
+            </button>
+
+			<br /> <p className={styles.Text}>
 				Exclusively on <img src={Binance} /> <b>Binance Smart Chain</b>
 			</p>
 			<br />
