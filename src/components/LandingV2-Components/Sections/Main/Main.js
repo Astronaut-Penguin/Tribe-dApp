24e5d35064209a79023d18ed0723fd164c41d103
Alejo Viola <viola.alejo@outlook.com>
2022-01-04 13:20:23 -0300

    Adding flex tags

--- src/components/LandingV2-Components/Sections/Main/Main.js
+++ src/components/LandingV2-Components/Sections/Main/Main.js
@@ -11,34 +11,30 @@ import Telegram from './assets/Telegram.png';
 import Twitter from './assets/Twitter.png';
 import Pancake from './assets/Pankake.png';
 import Bsc from './assets/bsc-8 2.png';
-import PanButton from './assets/PanButton.png'
+import PanButton from './assets/PanButton.png';
 
 const MainSection = () => {
 	return (
 		<section className={styles.Container}>
-			<div className={styles.Celebrities}>
-				Celebrities <span>TribePop</span> on the Metaverse
+			<div className={styles.Content}>
+				<div className={styles.Celebrities}>
+					Celebrities <span>TribePop</span> on the Metaverse
+				</div>
+				<p className={styles.Text}>
+					We launch NFTs. <br />
+					We build the future.
+				</p>
+				<img className={styles.Characters} src={PlebCharacter3} />
+				<p className={styles.Text}>
+					Buy <b>TRIBE Tokens</b> on
+				</p>
+				<button className={styles.PancakeButton}>
+					<img src={PanButton} /> <b>PankakeSwap</b>
+				</button>{' '}
+				<p className={styles.Text}>
+					Exclusively on <img src={Binance} /> <b>Binance Smart Chain</b>
+				</p>
 			</div>
-
-			<p className={styles.Text}>
-				We launch NFTs. <br />
-				We build the future.
-			</p>
-			<img className={styles.Characters} src={PlebCharacter3} />
-
-			<p className={styles.Text}>
-				Buy <b>TRIBE Tokens</b> on
-			</p><br />
-			
-            <button className={styles.PancakeButton}> 
-                <img src={PanButton} /> <b>PankakeSwap</b>
-            </button>
-
-			<br /> <p className={styles.Text}>
-				Exclusively on <img src={Binance} /> <b>Binance Smart Chain</b>
-			</p>
-			<br /> 
-
 			<div className={styles.Images}>
 				<a title="Telegram" href="/" className={styles.Social}>
 					<img src={Telegram} alt="Telegram" />
