73b329d277aac6f8d9febc3e288b511d387b4479
FacundoRago <facundorago98@gmail.com>
2022-01-04 15:14:30 -0300

    Fixed the position and width
    Ready to merge, working in all views

--- src/components/LandingV2-Components/Sections/Main/Main.js
+++ src/components/LandingV2-Components/Sections/Main/Main.js
@@ -6,6 +6,7 @@ import styles from './Main.module.css';
 
 //- Import Images
 import PlebCharacter3 from './assets/PlebCharacter3.png';
+import LargePlebCharacter3 from './assets/LargePlebCharacter3.png';
 import Binance from './assets/Binance.png';
 import Telegram from './assets/Telegram.png';
 import Twitter from './assets/Twitter.png';
@@ -16,38 +17,43 @@ import PanButton from './assets/PanButton.png';
 const MainSection = () => {
 	return (
 		<section className={styles.Container}>
-			<div className={styles.Content}>
-				<div className={styles.Celebrities}>
-					Celebrities <span>TribePop</span> on the Metaverse
+			<div className={styles.Subcontainer}>
+				<img className={styles.CharactersLarge} src={LargePlebCharacter3} />
+				<div className={styles.Content}>
+					<div className={styles.Celebrities}>
+						Celebrities  
+							<span className={styles.GradientText}> TribePop </span> 
+						 on the Metaverse
+					</div>
+					<p className={styles.Text} style={{marginTop: '15px'}}>
+						We launch NFTs. <br />
+						We build the future.
+					</p>
+					<img className={styles.Characters} src={PlebCharacter3} />
+					<p className={styles.TribeText}>
+						Buy <b>TRIBE Tokens</b> on
+					</p>
+					<button className={styles.PancakeButton}>
+						<img src={PanButton} /> <b>PancakeSwap</b>
+					</button>{' '}
+					<p className={styles.Text} style={{marginTop: '8px'}}>
+						Exclusively on <img src={Binance} /> <b>Binance Smart Chain</b>
+					</p>
 				</div>
-				<p className={styles.Text}>
-					We launch NFTs. <br />
-					We build the future.
-				</p>
-				<img className={styles.Characters} src={PlebCharacter3} />
-				<p className={styles.Text}>
-					Buy <b>TRIBE Tokens</b> on
-				</p>
-				<button className={styles.PancakeButton}>
-					<img src={PanButton} /> <b>PankakeSwap</b>
-				</button>{' '}
-				<p className={styles.Text}>
-					Exclusively on <img src={Binance} /> <b>Binance Smart Chain</b>
-				</p>
-			</div>
-			<div className={styles.Images}>
-				<a title="Telegram" href="/" className={styles.Social}>
-					<img src={Telegram} alt="Telegram" />
-				</a>
-				<a title="Twitter" href="/" className={styles.Social}>
-					<img src={Twitter} alt="Twitter" />
-				</a>
-				<a title="Pancake" href="/" className={styles.Social}>
-					<img src={Pancake} alt="Pancake" />
-				</a>
-				<a title="BSCScan" href="/" className={styles.Social}>
-					<img src={Bsc} alt="BSCScan" />
-				</a>
+				<div className={styles.Images}>
+					<a title="Telegram" href="/" className={styles.Social}>
+						<img src={Telegram} alt="Telegram" />
+					</a>
+					<a title="Twitter" href="/" className={styles.Social}>
+						<img src={Twitter} alt="Twitter" />
+					</a>
+					<a title="Pancake" href="/" className={styles.Social}>
+						<img src={Pancake} alt="Pancake" />
+					</a>
+					<a title="BSCScan" href="/" className={styles.Social}>
+						<img src={Bsc} alt="BSCScan" />
+					</a>
+				</div>
 			</div>
 		</section>
 	);
