9f72d8c092fb932973f7f052f17d8f2232032243
Alejo Viola <viola.alejo@outlook.com>
2022-01-04 12:50:28 -0300

    Structuring Header

--- src/components/LandingV2-Components/Header/Header.js
+++ src/components/LandingV2-Components/Header/Header.js
@@ -4,12 +4,20 @@ import React from 'react';
 //- Styles Imports
 import styles from './Header.module.css';
 
+//- Components Imports
+import { FlatButton } from '../..';
+
 const Header = () => {
 	return (
 		<header className={styles.Container}>
-			<a className={styles.Logo} href="">
-				Tribe
-			</a>
+			<div className={styles.Subcontainer}>
+				<a className={styles.Logo} href="">
+					Tribe
+				</a>
+				<div className={styles.Button}>
+					<FlatButton text={'Enter Application'} mid />
+				</div>
+			</div>
 		</header>
 	);
 };
