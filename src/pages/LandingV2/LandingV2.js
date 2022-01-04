fb28b0aea1b62c8c4815bf5412842159897e0850
FacundoRago <facundorago98@gmail.com>
2022-01-04 12:49:06 -0300

    Fixed some changes in Tablet view

--- src/pages/LandingV2/LandingV2.js
+++ src/pages/LandingV2/LandingV2.js
@@ -4,10 +4,13 @@ import React from 'react';
 //- Style Imports
 import styles from './LandingV2.module.css';
 
+import MainSection from '../../components/LandingV2-Components/Sections/Main/Main';
+
 const LandingV2 = () => {
 	return (
 		<>
-			<div className={styles.Background} />
+			{/* <div className={styles.Background} /> */}
+			<MainSection />
 		</>
 	);
 };
