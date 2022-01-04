050aa7a4ba76736958778baf90f775c451e3a049
FedeMar13 <federicojaviermarquez@hotmail.com>
2022-01-04 15:20:31 -0300

    add icon play

--- src/components/Cards/CardNFT/CardNFT.js
+++ src/components/Cards/CardNFT/CardNFT.js
@@ -23,6 +23,7 @@ const CardNFT = ({ onClick, name, colection, price, image, video, href }) => {
 			{video !== 'undefined' && (
 				<div className={styles.VideoContainer}>
 					<ImageLoader video={video} quite />
+					<svg xmlns="http://www.w3.org/2000/svg" className={styles.ButtonPlayNFT} height="38px" viewBox="0 0 24 24" width="38px" fill="#FFFFFF"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M10 8.64L15.27 12 10 15.36V8.64M8 5v14l11-7L8 5z"/></svg>
 					<BuyButton text="Buy" onClick={onClick} />
 				</div>
 			)}
