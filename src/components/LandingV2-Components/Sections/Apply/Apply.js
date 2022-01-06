20b9d61d05d33d6c4eb4f5fcd90d8db3b0b4b592
FacundoRago <facundorago98@gmail.com>
2022-01-06 14:26:58 -0300

    ApplySection updated
    Mobile view is ready, yet to finish the others

--- src/components/LandingV2-Components/Sections/Apply/Apply.js
+++ src/components/LandingV2-Components/Sections/Apply/Apply.js
@@ -6,7 +6,7 @@ import styles from './Apply.module.css';
 
 //- Import Components
 import CardNFT from '../../Cards/CardNFT/CardNFT';
-import ApplyButton from '../../Buttons/FlatButton/FlatButton';
+import ApplyButton from '../../Buttons/ApplyButton/ApplyButton';
 
 
 const ApplySection = () => {
@@ -14,20 +14,24 @@ const ApplySection = () => {
 		<section className={styles.Container}>
 			<div className={styles.Subcontainer}>
 				<div className={styles.Content}>
-                    <CardNFT 
-                        name={'The Pleb'}
-                        colection={'Caveman NFT'}
-                        price={'0.09 BNB'}
-                        image={'./assets/memepad/nft/Pleb.png'}
-                    />
-					<div className={styles.TribePop}>
-						Apply for your<span className={styles.GradientText}> TribePop NFTs </span>
-					</div>
-					<p className={styles.Text}>
-						Be an important part of our Marketplace, <br />
-                        share your creations and shape you future.
-					</p>
-                    <ApplyButton text={'Apply Now'}/>
+                    <div className={styles.CardContainer}>
+                        <CardNFT 
+                            name={'The Pleb'}
+                            colection={'Caveman NFT'}
+                            price={'0.09 BNB'}
+                            image={'./assets/memepad/nft/Pleb.png'}
+                        />
+                    </div>
+                    <div className={styles.TribePop}>
+                        Apply for your<span className={styles.GradientText}> TribePop NFTs </span>
+                    </div>
+                    <p className={styles.Text}>
+                        Be an important part of our Marketplace, <br />
+                        share your creations and shape your future.
+                    </p>
+                    <div className={styles.ButtonContainer}>
+                        <ApplyButton text={'Apply Now'}/>
+                    </div>
 				</div>
 			</div>
 		</section>
