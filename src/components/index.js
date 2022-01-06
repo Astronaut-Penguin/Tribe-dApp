90b0aaf4ad4edbc23f64cdbc609bf979eb7618b5
Alejo Viola <viola.alejo@outlook.com>
2022-01-06 11:37:23 -0300

    Fixing Export File

--- src/components/index.js
+++ src/components/index.js
@@ -6,7 +6,6 @@
 // Sections
 export { default as FeatureSection } from './LandingV2-Components/Sections/Features/Features';
 export { default as MainSection } from './LandingV2-Components/Sections/Main/Main';
-export { default as FeatureSection } from './LandingV2-Components/Sections/Features/Features';
 export { default as TribepopSection } from './LandingV2-Components/Sections/Tribepop/Tribepop';
 
 // Buttons
