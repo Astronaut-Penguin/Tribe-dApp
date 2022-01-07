01e9553cb869924ea339f7255ef4ec0e71fe7be8
Alejo Viola <viola.alejo@outlook.com>
2022-01-07 12:53:29 -0300

    Adding value on Header on LandingV2

--- src/pages/LandingV2/LandingV2.js
+++ src/pages/LandingV2/LandingV2.js
@@ -4,6 +4,8 @@ import React from 'react';
 //- Style Imports
 import styles from './LandingV2.module.css';
 
+//- Header Imports
+import { Header } from '../../components';
 //- Sections Imports
 import {
 	MainSection,
@@ -15,7 +17,7 @@ import {
 const LandingV2 = () => {
 	return (
 		<>
-			<Header />
+			<Header selected={0} />
 			<div className={styles.Container}>
 				<MainSection />
 				<FeatureSection />
