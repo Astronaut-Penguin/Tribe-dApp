8a64d47dc053ef05800268f626580fef88db2c18
Alejo Viola <viola.alejo@outlook.com>
2022-01-07 11:30:55 -0300

    Fixing Apply.js

--- src/components/LandingV2-Components/Sections/Apply/Apply.js
+++ src/components/LandingV2-Components/Sections/Apply/Apply.js
@@ -8,28 +8,28 @@ import styles from './Apply.module.css';
 import CardNFT from '../../Cards/CardNFT/CardNFT';
 import ApplyButton from '../../Buttons/ApplyButton/ApplyButton';
 
-
 const ApplySection = () => {
 	return (
 		<section className={styles.Container}>
 			<div className={styles.Subcontainer}>
+				<CardNFT
+					name={'The Pleb'}
+					colection={'Caveman NFT'}
+					price={'0.09 BNB'}
+					image={'./assets/memepad/nft/Pleb.png'}
+				/>
 				<div className={styles.Content}>
-                    <CardNFT 
-                        name={'The Pleb'}
-                        colection={'Caveman NFT'}
-                        price={'0.09 BNB'}
-                        image={'./assets/memepad/nft/Pleb.png'}
-                    />
-                    <div className={styles.TribePop}>
-                        Apply for your<span className={styles.GradientText}> TribePop NFTs </span>
-                    </div>
-                    <p className={styles.Text}>
-                        Be an important part of our Marketplace, <br />
-                        share your creations and shape your future.
-                    </p>
-                    <div className={styles.ButtonContainer}>
-                        <ApplyButton text={'Apply Now'}/>
-                    </div>
+					<div className={styles.TribePop}>
+						Apply for your
+						<span className={styles.GradientText}> TribePop NFTs </span>
+					</div>
+					<p className={styles.Text}>
+						Be an important part of our Marketplace, <br />
+						share your creations and shape your future.
+					</p>
+					<div className={styles.ButtonContainer}>
+						<ApplyButton text={'Apply Now'} />
+					</div>
 				</div>
 			</div>
 		</section>
