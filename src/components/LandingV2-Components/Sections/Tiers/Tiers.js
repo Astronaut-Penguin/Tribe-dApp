aaf385386c2d11bc8d54e4f542e079ce2b85f47e
FacundoRago <facundorago98@gmail.com>
2022-01-11 12:11:43 -0300

    Fixed some bugs and added some changes
    Tiers Section is ready on PC View, yet to add the carousel for mobile views.

--- src/components/LandingV2-Components/Sections/Tiers/Tiers.js
+++ src/components/LandingV2-Components/Sections/Tiers/Tiers.js
@@ -6,9 +6,6 @@ import styles from './Tiers.module.css';
 
 //- Import Card
 import CardTier from '../../Cards/CardTier/CardTier';
-import CardTier2 from '../../Cards/CardTier/CardTier';
-import CardTier3 from '../../Cards/CardTier/CardTier';
-
 
 //- Import Images
 import Caveman from './assets/Caveman.png';
@@ -20,12 +17,39 @@ import GladiatorLogo from './assets/GladiatorLogo.png';
 
 const TiersSection = () => {
     return (
-        <CardTier 
-            price={'50,000'} 
-            allocation={'Guaranteed Allocation'} 
-            image={Gladiator} 
-            logo={GladiatorLogo} 
-        />
+        <div className={styles.CardContainer}>
+            <h2 className={styles.Tiers}>Tiers</h2>
+            <div className={styles.Card}>
+                <CardTier 
+                    onClick={'/'} 
+                    price={'2,500'} 
+                    allocation={'5 out of 10'} 
+                    image={Caveman} 
+                    logo={CavemanLogo} 
+                />
+                <h3>Access to Level 1 <br/> NFTs</h3>
+            </div>
+            <div className={styles.Card}>
+                <CardTier 
+                    onClick={'/'} 
+                    price={'50,000'} 
+                    allocation={'Guaranteed Allocation'} 
+                    image={CryptoKing} 
+                    logo={CryptoKingLogo} 
+                />
+                <h3>Access to Exclusive <br/> NFTs</h3>
+            </div>
+            <div className={styles.Card}>
+                <CardTier 
+                    onClick={'/'} 
+                    price={'17,500'} 
+                    allocation={'7 out of 10'} 
+                    image={Gladiator} 
+                    logo={GladiatorLogo} 
+                />
+                <h3>Access to Level 2 <br/> NFTs</h3>
+            </div>
+        </div>
     );
 };
 
