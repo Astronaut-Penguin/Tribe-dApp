625c07f141f77da5abe769a3249c92fc52b3ac72
Alejo Viola <viola.alejo@outlook.com>
2022-01-14 11:10:29 -0300

    Adding OwlCarousel

--- src/components/LandingV2-Components/Sections/Tiers/Tiers.js
+++ src/components/LandingV2-Components/Sections/Tiers/Tiers.js
@@ -15,42 +15,76 @@ import CryptoKingLogo from './assets/CryptoKingLogo.png';
 import Gladiator from './assets/Gladiator.png';
 import GladiatorLogo from './assets/GladiatorLogo.png';
 
+//- Owl Carousel Imports
+import OwlCarousel from 'react-owl-carousel';
+import 'owl.carousel/dist/assets/owl.carousel.css';
+import 'owl.carousel/dist/assets/owl.theme.default.css';
+
 const TiersSection = () => {
-    return (
-        <div className={styles.CardContainer}>
-            <h2 className={styles.Tiers}>Tiers</h2>
-            <div className={styles.Card}>
-                <CardTier 
-                    onClick={'/'} 
-                    price={'2,500'} 
-                    allocation={'5 out of 10'} 
-                    image={Caveman} 
-                    logo={CavemanLogo} 
-                />
-                <h3>Access to Level 1 <br/> NFTs</h3>
-            </div>
-            <div className={styles.Card}>
-                <CardTier 
-                    onClick={'/'} 
-                    price={'50,000'} 
-                    allocation={'Guaranteed Allocation'} 
-                    image={CryptoKing} 
-                    logo={CryptoKingLogo} 
-                />
-                <h3>Access to Exclusive <br/> NFTs</h3>
-            </div>
-            <div className={styles.Card}>
-                <CardTier 
-                    onClick={'/'} 
-                    price={'17,500'} 
-                    allocation={'7 out of 10'} 
-                    image={Gladiator} 
-                    logo={GladiatorLogo} 
-                />
-                <h3>Access to Level 2 <br/> NFTs</h3>
-            </div>
-        </div>
-    );
+	const options = {
+		center: true,
+		margin: 10,
+		autoWidth: true,
+
+		URLhashListener: true,
+		startPosition: 'URLHash',
+	};
+
+	return (
+		<div className={styles.CardContainer}>
+			<h2 className={styles.Tiers}>Tiers</h2>
+			<div className={styles.Card}>
+				<CardTier
+					onClick={'/'}
+					price={'2,500'}
+					allocation={'5 out of 10'}
+					image={Caveman}
+					logo={CavemanLogo}
+				/>
+				<h3>
+					Access to Level 1 <br /> NFTs
+				</h3>
+			</div>
+			<div className={styles.Card}>
+				<CardTier
+					onClick={'/'}
+					price={'50,000'}
+					allocation={'Guaranteed Allocation'}
+					image={CryptoKing}
+					logo={CryptoKingLogo}
+				/>
+				<h3>
+					Access to Exclusive <br /> NFTs
+				</h3>
+			</div>
+			<div className={styles.Card}>
+				<CardTier
+					onClick={'/'}
+					price={'17,500'}
+					allocation={'7 out of 10'}
+					image={Gladiator}
+					logo={GladiatorLogo}
+				/>
+				<h3>
+					Access to Level 2 <br /> NFTs
+				</h3>
+			</div>
+			<OwlCarousel {...options} className={styles.NavContainer}>
+				<div>Faso</div>
+				<div>Faso</div>
+				<div>Faso</div>
+				<div>Faso</div>
+				<div>Faso</div>
+				<div>Faso</div>
+				<div>Faso</div>
+				<div>Faso</div>
+				<div>Faso</div>
+				<div>Faso</div>
+				<div>Faso</div>
+				<div>Faso</div>
+			</OwlCarousel>
+		</div>
+	);
 };
 
 export default TiersSection;
