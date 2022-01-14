653ffe9992788d46cd408d7a978571b3041f00e1
FacundoRago <facundorago98@gmail.com>
2022-01-14 18:24:05 -0300

    Minor fixes to Tiers Section
    Yet to fix the views

--- src/components/LandingV2-Components/Sections/Tiers/Tiers.js
+++ src/components/LandingV2-Components/Sections/Tiers/Tiers.js
@@ -25,36 +25,15 @@ import Flicking from '@egjs/react-flicking';
 import '@egjs/react-flicking/dist/flicking.css';
 
 const TiersSection = () => {
-	const options = {
-		items: 3,
-		loop: true,
-		center: true,
-		pullDrag: false,
-
-		startPosition: 1,
-		autoWidth: true,
-	};
-
 	return (
 		<div className={styles.CardContainer}>
+			<div className={styles.TiersContainer}>
+				<h2 className={styles.Tiers}>Tiers</h2>
+			</div>
 			<Flicking
 				circular={true}
-				onChanged={(e) => {
-					console.log(e.index);
-				}}
 			>
-				<a href="">Pito</a>
-				<a href="">Puto</a>
-				<a href="">Faso</a>
-				<a href="">Feso</a>
-				<a href="">Fiso</a>
-				<a href="">Foso</a>
-				<a href="">Fuso</a>
-			</Flicking>
-			<h2 className={styles.Tiers}>Tiers</h2>
-			<OwlCarousel {...options} className={styles.NavContainer}>
-				<div className={'item'}>
-					<div className={styles.Card}>
+				<div className={styles.Card}>
 						<CardTier
 							onClick={'/'}
 							price={'2,500'}
@@ -65,37 +44,32 @@ const TiersSection = () => {
 						<h3>
 							Access to Level 1 <br /> NFTs
 						</h3>
-					</div>
 				</div>
-				<div className={'item'}>
-					<div className={styles.Card}>
-						<CardTier
-							onClick={'/'}
-							price={'50,000'}
-							allocation={'Guaranteed Allocation'}
-							image={CryptoKing}
-							logo={CryptoKingLogo}
-						/>
-						<h3>
-							Access to Exclusive <br /> NFTs
-						</h3>
-					</div>
+				<div className={styles.Card}>
+					<CardTier
+						onClick={'/'}
+						price={'50,000'}
+						allocation={'Guaranteed Allocation'}
+						image={CryptoKing}
+						logo={CryptoKingLogo}
+					/>
+					<h3>
+						Access to Exclusive <br /> NFTs
+					</h3>
 				</div>
-				<div className={'item'}>
-					<div className={styles.Card}>
-						<CardTier
-							onClick={'/'}
-							price={'17,500'}
-							allocation={'7 out of 10'}
-							image={Gladiator}
-							logo={GladiatorLogo}
-						/>
-						<h3>
-							Access to Level 2 <br /> NFTs
-						</h3>
-					</div>
+				<div className={styles.Card}>
+					<CardTier
+						onClick={'/'}
+						price={'17,500'}
+						allocation={'7 out of 10'}
+						image={Gladiator}
+						logo={GladiatorLogo}
+					/>
+					<h3>
+						Access to Level 2 <br /> NFTs
+					</h3>
 				</div>
-			</OwlCarousel>
+			</Flicking>			
 		</div>
 	);
 };
