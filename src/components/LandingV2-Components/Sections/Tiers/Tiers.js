a66d31d7870c5334b124305ae90b28940079d368
Alejo Viola <viola.alejo@outlook.com>
2022-01-14 16:03:58 -0300

    Adding Flicking on Tiers.js

--- src/components/LandingV2-Components/Sections/Tiers/Tiers.js
+++ src/components/LandingV2-Components/Sections/Tiers/Tiers.js
@@ -15,10 +15,14 @@ import CryptoKingLogo from './assets/CryptoKingLogo.png';
 import Gladiator from './assets/Gladiator.png';
 import GladiatorLogo from './assets/GladiatorLogo.png';
 
-//- Owl Carousel Imports
+//- Owl Carousel Imports (Borrarlo si no se usa)
 import OwlCarousel from 'react-owl-carousel';
 import 'owl.carousel/dist/assets/owl.carousel.css';
 import 'owl.carousel/dist/assets/owl.theme.default.css';
+
+//- Flicking Imporst
+import Flicking from '@egjs/react-flicking';
+import '@egjs/react-flicking/dist/flicking.css';
 
 const TiersSection = () => {
 	const options = {
@@ -27,57 +31,70 @@ const TiersSection = () => {
 		center: true,
 		pullDrag: false,
 
-		margin: 10,
 		startPosition: 1,
 		autoWidth: true,
 	};
 
 	return (
 		<div className={styles.CardContainer}>
+			<Flicking
+				circular={true}
+				onChanged={(e) => {
+					console.log(e.index);
+				}}
+			>
+				<a href="">Pito</a>
+				<a href="">Puto</a>
+				<a href="">Faso</a>
+				<a href="">Feso</a>
+				<a href="">Fiso</a>
+				<a href="">Foso</a>
+				<a href="">Fuso</a>
+			</Flicking>
 			<h2 className={styles.Tiers}>Tiers</h2>
 			<OwlCarousel {...options} className={styles.NavContainer}>
-			<div className={'item'}>
-				<div className={styles.Card}>
-					<CardTier
-						onClick={'/'}
-						price={'2,500'}
-						allocation={'5 out of 10'}
-						image={Caveman}
-						logo={CavemanLogo}
-					/>
-					<h3>
-						Access to Level 1 <br /> NFTs
-					</h3>
+				<div className={'item'}>
+					<div className={styles.Card}>
+						<CardTier
+							onClick={'/'}
+							price={'2,500'}
+							allocation={'5 out of 10'}
+							image={Caveman}
+							logo={CavemanLogo}
+						/>
+						<h3>
+							Access to Level 1 <br /> NFTs
+						</h3>
+					</div>
 				</div>
-			</div>
-			<div className={'item'}>
-				<div className={styles.Card}>
-					<CardTier
-						onClick={'/'}
-						price={'50,000'}
-						allocation={'Guaranteed Allocation'}
-						image={CryptoKing}
-						logo={CryptoKingLogo}
-					/>
-					<h3>
-						Access to Exclusive <br /> NFTs
-					</h3>
+				<div className={'item'}>
+					<div className={styles.Card}>
+						<CardTier
+							onClick={'/'}
+							price={'50,000'}
+							allocation={'Guaranteed Allocation'}
+							image={CryptoKing}
+							logo={CryptoKingLogo}
+						/>
+						<h3>
+							Access to Exclusive <br /> NFTs
+						</h3>
+					</div>
 				</div>
-			</div>
-			<div className={'item'}>
-				<div className={styles.Card}>
-					<CardTier
-						onClick={'/'}
-						price={'17,500'}
-						allocation={'7 out of 10'}
-						image={Gladiator}
-						logo={GladiatorLogo}
-					/>
-					<h3>
-						Access to Level 2 <br /> NFTs
-					</h3>
+				<div className={'item'}>
+					<div className={styles.Card}>
+						<CardTier
+							onClick={'/'}
+							price={'17,500'}
+							allocation={'7 out of 10'}
+							image={Gladiator}
+							logo={GladiatorLogo}
+						/>
+						<h3>
+							Access to Level 2 <br /> NFTs
+						</h3>
+					</div>
 				</div>
-			</div>
 			</OwlCarousel>
 		</div>
 	);
