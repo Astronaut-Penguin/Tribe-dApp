402b73468af3a1accbf17e9de2e118e36b072ecc
Alejo Viola <viola.alejo@outlook.com>
2022-01-15 17:39:16 -0300

    Separating Item List on mobile and pc

--- src/components/LandingV2-Components/Header/Header.js
+++ src/components/LandingV2-Components/Header/Header.js
@@ -10,21 +10,23 @@ import styles from './Header.module.css';
 import { Link } from 'react-router-dom';
 import { FlatButton } from '../..';
 import MobileNavBar from '../NavBar/MobileNavBar/MobileNavBar';
+import NavBar from '../NavBar/DefaultNavBar/NavBar';
 
-const Header = ({ sections }) => {
+const Header = ({ mobile, sections }) => {
 	return (
 		<header className={styles.Container}>
 			<div className={styles.Subcontainer}>
 				<a className={styles.Logo} href="">
 					Tribe
 				</a>
+				<NavBar sections={sections} />
 				<div className={styles.Button}>
 					<Link to="/dashboard">
 						<FlatButton mid text={'Enter Application'} />
 					</Link>
 				</div>
 			</div>
-			<MobileNavBar sections={sections} />
+			<MobileNavBar sections={mobile} />
 		</header>
 	);
 };
