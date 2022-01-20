aa32f9f73a1d24f8534f67701ebb18ba2c642c26
Alejo Viola <viola.alejo@outlook.com>
2022-01-20 18:32:30 -0300

    [Landing v2] - [FaqSection] - Fixing Faq Section

--- src/components/LandingV2-Components/Sections/FAQ/Data.js
+++ src/components/LandingV2-Components/Sections/FAQ/Data.js
@@ -1,18 +1,22 @@
-export const Data = [
-    {
-      question: 'Where do I buy TRIBEX?',
-      answer: 'No ni idea pa'
-    },
-    {
-      question: 'How to earn rewards by stacking?',
-      answer: 'De nuevo me mataste maestro'
-    },
-    {
-      question: 'How do I buy Tribe NFTs?',
-      answer: 'Si te digo te miento'
-    },
-    {
-        question: "I'm a Celebrity / Brand, How do I launch my own NFT?",
-        answer: 'Esto es una prueba de texto largo, porque no tenía ninguno para poner viste, asi que estoy viendo como se comporta un texto de varias lineas en este hermoso y funcional acordeón, mi tío abuelo toca el acordeón, es un crack el viejo.'
-    }
-  ];
+export const data = [
+	{
+		question: 'Where do I buy TRIBEX?',
+		answer:
+			'Currently you can get TRIBEX through PancakeSwap by buying with BNB.',
+	},
+	{
+		question: 'How to earn rewards by staking?',
+		answer:
+			'You simply have to visit our staking platform, choose a pool, approve the contract and deposit the amount of TRIBEX you want.',
+	},
+	{
+		question: 'How do I buy TribePops?',
+		answer:
+			'In order to participate in the TribePops sale, you must be staking TRIBEX in the staking pool. You can also access the "Open Edition" TribePop that does not require TRIBEX staking.',
+	},
+	{
+		question: "I'm a Celebrity / Brand, How do I launch my own TribePop?",
+		answer:
+			'If you want to launch your own collection of TribePops, simply email us at Info@TribeToken.app or contact one of the moderators at T.me/TribeEN',
+	},
+];
--- src/components/LandingV2-Components/Sections/FAQ/Faq.js
+++ src/components/LandingV2-Components/Sections/FAQ/Faq.js
@@ -1,55 +1,28 @@
 //- Import React
-import React, { useState } from 'react';
-import { Data } from './Data';
-import { IconContext } from 'react-icons';
+import React from 'react';
+import { data } from './Data';
 
 //- Import Styles
 import styles from './Faq.module.css';
 
-//- Import Icons
-import Triangle from './assets/Triangle.png';
-import TriangleOpen from './assets/TriangleOpen.png';
+//- Components Imports
+import Question from './Question';
 
 const FaqSection = () => {
-  const [clicked, setClicked] = useState(false);
-
-  const toggle = index => {
-    if (clicked === index) {
-      return setClicked(null);
-    }
-
-    setClicked(index);
-  };
-
-  return (
-    <IconContext.Provider value={{ color: '#00FFB9', size: '25px' }}>
-      <h2 className={styles.Faq}>Frequently Asked Questions</h2>
-      <div className={styles.AccordionSection}>
-        <div className={styles.Container}>
-          {Data.map((item, index) => {
-            return (
-              <>
-                <div className={styles.Wrap} onClick={() => toggle(index)} key={index}>
-                  <span>
-                    {clicked === index ? 
-                      <img className={styles.Img} src={TriangleOpen} /> : 
-                      <img className={styles.Img} src={Triangle} />}
-                  </span>
-                  <h1>{item.question}</h1>
-                </div>
-                {clicked === index ? (
-                  <div className={styles.Dropdown}>
-                    <p>{item.answer}</p>
-                  </div>
-                ) : null}
-                <div className={styles.Border}/>
-              </>
-            );
-          })}
-        </div>
-      </div>
-    </IconContext.Provider>
-  );
+	console.log(data);
+	return (
+		<section className={styles.Container}>
+			<h2 className={styles.Title}>Frequently Asked Questions</h2>
+			{data.map((value, i) => {
+				return (
+					<>
+						<Question q={value.question} r={value.answer} />
+						<hr className={styles.Hr} />
+					</>
+				);
+			})}
+		</section>
+	);
 };
 
 export default FaqSection;
