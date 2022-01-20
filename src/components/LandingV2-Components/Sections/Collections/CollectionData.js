54e5d6b2a091336df2ab960fe8407a23513a6ae2
FacundoRago <facundorago98@gmail.com>
2022-01-19 22:30:05 -0300

    Added minor features to Collections Section

--- src/components/LandingV2-Components/Sections/Collections/CollectionData.js
+++ src/components/LandingV2-Components/Sections/Collections/CollectionData.js
@@ -1,12 +1,19 @@
 //- DATA ARRAY
 
-//- Import Images
-import MemepadLogo from './assets/MemepadLogo.png';
-
-const CollectionsData = [
-    {img: {MemepadLogo}, title: 'Memepad', text: 'Every brand, athlete, singer, model, actor, among others, that works with TRIBE, has their own TribePop adapted to different situations in their career to create their NFTs.'},
-    {img: {MemepadLogo}, title: 'Memepad2', text: 'Every brand, athlete, singer, model, actor, among others, that works with TRIBE, has their own TribePop adapted to different situations in their career to create their NFTs.'},
-    {img: {MemepadLogo}, title: 'Memepad3', text: 'Every brand, athlete, singer, model, actor, among others, that works with TRIBE, has their own TribePop adapted to different situations in their career to create their NFTs.'},
-]
-
-export default CollectionsData;
+export const CollectionsData = [
+    {
+        img: './assets/MemepadLogo.png', 
+        title: 'Memepad', 
+        text: 'Every brand, athlete, singer, model, actor, among others, that works with TRIBE, has their own TribePop adapted to different situations in their career to create their NFTs.'
+    },
+    {
+        img: './assets/MemepadLogo.png', 
+        title: 'Memepad2', 
+        text: 'Every brand, athlete, singer, model, actor, among others, that works with TRIBE, has their own TribePop adapted to different situations in their career to create their NFTs.'
+    },
+    {
+        img: './assets/MemepadLogo.png', 
+        title: 'Memepad3', 
+        text: 'Every brand, athlete, singer, model, actor, among others, that works with TRIBE, has their own TribePop adapted to different situations in their career to create their NFTs.'
+    },
+];
--- src/components/LandingV2-Components/Sections/Collections/Collections.js
+++ src/components/LandingV2-Components/Sections/Collections/Collections.js
@@ -3,6 +3,7 @@ import React, { useEffect, useState, createRef } from 'react';
 
 //- Import Styles
 import styles from './Collections.module.css';
+import './FlickingStyles.module.css';
 
 //- Import Data
 import MemepadLogo from './assets/MemepadLogo.png';
@@ -12,57 +13,72 @@ import CollectionData from './CollectionData';
 import ApplyButton from '../../Buttons/ApplyButton/ApplyButton';
 
 //- Flicking Imports
-import Flicking from '@egjs/react-flicking';
 import '@egjs/react-flicking/dist/flicking.css';
-import Dots from 'react-carousel-dots';
+import "@egjs/flicking-plugins/dist/flicking-plugins.css";
+import Flicking, { ViewportSlot } from "@egjs/react-flicking";
+import { Pagination } from "@egjs/flicking-plugins";
+import "@egjs/flicking-plugins/dist/pagination.css";
+import { Arrow } from "@egjs/flicking-plugins";
+import "@egjs/flicking-plugins/dist/arrow.css";
 
 const CollectionsSection = ({ CollectionData }) => {
 
 	//- SELECTED STATE
-	const [s, setS] = useState(0);
+	// const [s, setS] = useState(0);
+
+	const [plugins, setPlugins] = 
+		useState(
+			[new Pagination({ type: 'bullet' })], 
+			[new Arrow()],
+	);
 
 	////////////
 	// RENDER //
 	////////////
 
 	return (
-		<div className={styles.BigContainer}>
-			<div className={styles.TitleContainer}>
-				<h2 className={styles.Title}>Latest NFT Collections</h2>
+		<>
+			<div className={styles.BigContainer}>
+				<div className={styles.TitleContainer}>
+					<h2 className={styles.Title}>Latest NFT Collections</h2>
+				</div>
+				<Flicking
+					circular={true}
+					onChanged={(e) => {
+						// setS(e.index);
+						console.log(e.index);
+					}}
+					defaultIndex={1}
+					plugins={plugins}
+					className={styles.Carousel}
+				>
+					<div className={styles.Card}>
+						<img src={MemepadLogo} className={styles.Img} />
+					</div>
+					<div className={styles.Card}>
+						<img src={MemepadLogo} className={styles.Img} />
+					</div>
+					<div className={styles.Card}>
+						<img src={MemepadLogo} className={styles.Img} />
+					</div>
+					<ViewportSlot>
+						<div className={`flicking-pagination ${styles.Dots}`}></div>
+						<span className="flicking-arrow-prev"></span>
+        				<span className="flicking-arrow-next"></span>
+					</ViewportSlot>
+				</Flicking>
+				<h3>Memepad2</h3>
+				<h4>
+					Every brand, athlete, singer, model, actor, among <br />
+					others, that works with TRIBE, has their own <br/>
+					TribePop adapted to different situations in their <br/>
+					career to create their NFTs.
+				</h4>
+				<br/><div className={styles.ButtonContainer}>
+					<ApplyButton text={'View All Collections'} />
+				</div>
 			</div>
-			<Flicking
-				circular={true}
-				onChanged={(e) => {
-					setS(e.index);
-					console.log(e.index);
-				}}
-				defaultIndex={1}
-				className={styles.Carousel}
-			>
-				<div className={styles.Card}>
-                    <img src={MemepadLogo} className={styles.Img} />
-				</div>
-				<div className={styles.Card}>
-                    <img src={MemepadLogo} className={styles.Img} />
-				</div>
-				<div className={styles.Card}>
-					<img src={MemepadLogo} className={styles.Img} />
-				</div>
-			</Flicking>
-			<h3>Memepad2</h3>
-			<h4>
-                Every brand, athlete, singer, model, actor, among <br />
-                others, that works with TRIBE, has their own <br/>
-                TribePop adapted to different situations in their <br/>
-                career to create their NFTs.
-			</h4>
-			<div className={styles.DotsContainer}>
-				<Dots length={3} active={2} size={12} />
-			</div>
-            <br/><div className={styles.ButtonContainer}>
-                <ApplyButton text={'View All Collections'} />
-            </div>
-		</div>
+		</>
 	);
 };
 
