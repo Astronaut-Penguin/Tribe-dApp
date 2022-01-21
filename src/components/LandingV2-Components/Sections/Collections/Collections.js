aad1f6f6ef00dc223eaf26dac769d7022069e2d8
Alejo Viola <viola.alejo@outlook.com>
2022-01-21 17:32:43 -0300

    [Landing v2] - [Collections Section] - Fixing collections section

--- src/components/LandingV2-Components/Sections/Collections/Collections.js
+++ src/components/LandingV2-Components/Sections/Collections/Collections.js
@@ -16,23 +16,21 @@ import ApplyButton from '../../Buttons/ApplyButton/ApplyButton';
 
 //- Flicking Imports
 import '@egjs/react-flicking/dist/flicking.css';
-import "@egjs/flicking-plugins/dist/flicking-plugins.css";
-import Flicking, { ViewportSlot } from "@egjs/react-flicking";
-import { Pagination } from "@egjs/flicking-plugins";
-import "@egjs/flicking-plugins/dist/pagination.css";
-import { Arrow } from "@egjs/flicking-plugins";
-import "@egjs/flicking-plugins/dist/arrow.css";
+import '@egjs/flicking-plugins/dist/flicking-plugins.css';
+import Flicking, { ViewportSlot } from '@egjs/react-flicking';
+import { Pagination } from '@egjs/flicking-plugins';
+import '@egjs/flicking-plugins/dist/pagination.css';
+import { Arrow } from '@egjs/flicking-plugins';
+import '@egjs/flicking-plugins/dist/arrow.css';
 
 const CollectionsSection = ({ CollectionData }) => {
-
 	//- SELECTED STATE
-	// const [s, setS] = useState(0);
+	const [s, setS] = useState(0);
 
-	const [plugins, setPlugins] = 
-		useState(
-			[new Pagination({ type: 'bullet' })], 	
-			// [new Arrow()],
-	);
+	const [plugins, setPlugins] = useState([
+		new Pagination({ type: 'bullet' }),
+		new Arrow(),
+	]);
 
 	////////////
 	// RENDER //
@@ -47,7 +45,7 @@ const CollectionsSection = ({ CollectionData }) => {
 				<Flicking
 					circular={true}
 					onChanged={(e) => {
-						// setS(e.index);
+						setS(e.index);
 						console.log(e.index);
 					}}
 					defaultIndex={1}
@@ -55,30 +53,58 @@ const CollectionsSection = ({ CollectionData }) => {
 					className={styles.Carousel}
 				>
 					<div className={styles.Card}>
-						<img src={MemepadLogo} className={styles.Img} />
+						<img
+							src={MemepadLogo}
+							className={`${styles.Img} ${
+								s == 0 ? styles.Selected : styles.NotSelected
+							}`}
+						/>
 					</div>
 					<div className={styles.Card}>
-						<img src={MemepadLogo} className={styles.Img} />
+						<img
+							src={MemepadLogo}
+							className={`${styles.Img} ${
+								s == 1 ? styles.Selected : styles.NotSelected
+							}`}
+						/>
 					</div>
 					<div className={styles.Card}>
-						<img src={MemepadLogo} className={styles.Img} />
+						<img
+							src={MemepadLogo}
+							className={`${styles.Img} ${
+								s == 2 ? styles.Selected : styles.NotSelected
+							}`}
+						/>
+					</div>
+					<div className={styles.Card}>
+						<img
+							src={MemepadLogo}
+							className={`${styles.Img} ${
+								s == 3 ? styles.Selected : styles.NotSelected
+							}`}
+						/>
 					</div>
 					<ViewportSlot>
 						<div className={`flicking-pagination ${styles.Dots}`}></div>
 						<div className={styles.ArrowContainer}>
-							<span className="flicking-arrow-prev"><img className={styles.ArrowLeft} src={LeftArrow}/></span>
-							<span className="flicking-arrow-next"><img className={styles.ArrowRight} src={RightArrow}/></span>
+							<span className="flicking-arrow-prev">
+								<img className={styles.ArrowLeft} src={LeftArrow} />
+							</span>
+							<span className="flicking-arrow-next">
+								<img className={styles.ArrowRight} src={RightArrow} />
+							</span>
 						</div>
 					</ViewportSlot>
 				</Flicking>
 				<h3>Memepad2</h3>
 				<h4>
 					Every brand, athlete, singer, model, actor, among <br />
-					others, that works with TRIBE, has their own <br/>
-					TribePop adapted to different situations in their <br/>
+					others, that works with TRIBE, has their own <br />
+					TribePop adapted to different situations in their <br />
 					career to create their NFTs.
 				</h4>
-				<br/><div className={styles.ButtonContainer}>
+				<br />
+				<div className={styles.ButtonContainer}>
 					<ApplyButton text={'View All Collections'} />
 				</div>
 			</div>
