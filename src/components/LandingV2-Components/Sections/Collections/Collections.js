8d17391bd7efccb142a03578173b4279203e13ff
Alejo Viola <viola.alejo@outlook.com>
2022-01-23 19:10:26 -0300

    [Landing v2] - [Collections Section] - Fixing Collections

--- src/components/LandingV2-Components/Sections/Collections/Collections.js
+++ src/components/LandingV2-Components/Sections/Collections/Collections.js
@@ -3,7 +3,6 @@ import React, { useEffect, useState, createRef } from 'react';
 
 //- Import Styles
 import styles from './Collections.module.css';
-import './FlickingStyles.module.css';
 
 //- Import Data
 import MemepadLogo from './assets/MemepadLogo.png';
@@ -22,7 +21,12 @@ import '@egjs/flicking-plugins/dist/pagination.css';
 import { Arrow } from '@egjs/flicking-plugins';
 import '@egjs/flicking-plugins/dist/arrow.css';
 
-const CollectionsSection = () => {
+import './FlickingStyles.css';
+
+//- Data Imports
+import data from '../../../../data/artist.json';
+
+const CollectionsSection = ({ id }) => {
 	//- SELECTED STATE
 	const [s, setS] = useState(0);
 
@@ -37,7 +41,7 @@ const CollectionsSection = () => {
 
 	return (
 		<>
-			<div className={styles.BigContainer}>
+			<div className={styles.BigContainer} id={id}>
 				<div className={styles.TitleContainer}>
 					<h2 className={styles.Title}>Latest NFT Collections</h2>
 				</div>
@@ -50,39 +54,21 @@ const CollectionsSection = () => {
 					defaultIndex={0}
 					plugins={plugins}
 					className={styles.Carousel}
+					renderOnlyVisible={true}
 				>
-					<div className={styles.Card}>
-						<img
-							src={MemepadLogo}
-							className={`${styles.Img} ${
-								s == 0 ? styles.Selected : styles.NotSelected
-							}`}
-						/>
-					</div>
-					<div className={styles.Card}>
-						<img
-							src={MemepadLogo}
-							className={`${styles.Img} ${
-								s == 1 ? styles.Selected : styles.NotSelected
-							}`}
-						/>
-					</div>
-					<div className={styles.Card}>
-						<img
-							src={MemepadLogo}
-							className={`${styles.Img} ${
-								s == 2 ? styles.Selected : styles.NotSelected
-							}`}
-						/>
-					</div>
-					<div className={styles.Card}>
-						<img
-							src={MemepadLogo}
-							className={`${styles.Img} ${
-								s == 3 ? styles.Selected : styles.NotSelected
-							}`}
-						/>
-					</div>
+					{data.data.map((value, i) => {
+						return (
+							<div className={styles.Card}>
+								<img
+									src={value.collections.logo}
+									className={`${styles.Img} ${
+										s == i ? styles.Selected : styles.NotSelected
+									}`}
+								/>
+							</div>
+						);
+					})}
+
 					<ViewportSlot>
 						<div className={`flicking-pagination ${styles.Dots}`}></div>
 						<div className={styles.ArrowContainer}>
@@ -95,53 +81,22 @@ const CollectionsSection = () => {
 						</div>
 					</ViewportSlot>
 				</Flicking>
-				<div className={`${styles.TextContainer} ${
-					s == 0 ? styles.TextSelected : styles.TextNotSelected
-				}`}>
-					<h3>Memepad0</h3>
-					<h4>
-						Every brand, athlete, singer, model, actor, among <br />
-						others, that works with TRIBE, has their own <br />
-						TribePop adapted to different situations in their <br />
-						career to create their NFTs.
-					</h4>
-				</div>
-				<div className={`${styles.TextContainer} ${
-					s == 1 ? styles.TextSelected : styles.TextNotSelected
-				}`}>
-					<h3>Memepad1</h3>
-					<h4>
-						Every brand, athlete, singer, model, actor, among <br />
-						others, that works with TRIBE, has their own <br />
-						TribePop adapted to different situations in their <br />
-						career to create their NFTs.
-					</h4>
-				</div>
-				<div className={`${styles.TextContainer} ${
-					s == 2 ? styles.TextSelected : styles.TextNotSelected
-				}`}>
-					<h3>Memepad2</h3>
-					<h4>
-						Every brand, athlete, singer, model, actor, among <br />
-						others, that works with TRIBE, has their own <br />
-						TribePop adapted to different situations in their <br />
-						career to create their NFTs.
-					</h4>
-				</div>
-				<div className={`${styles.TextContainer} ${
-					s == 3 ? styles.TextSelected : styles.TextNotSelected
-				}`}>
-					<h3>Memepad3</h3>
-					<h4>
-						Every brand, athlete, singer, model, actor, among <br />
-						others, that works with TRIBE, has their own <br />
-						TribePop adapted to different situations in their <br />
-						career to create their NFTs.
-					</h4>
-				</div>
+				{data.data.map((value, i) => {
+					return (
+						<div
+							className={`${styles.TextContainer} ${
+								s == i ? styles.TextSelected : styles.TextNotSelected
+							}`}
+						>
+							<h3>{value.name}</h3>
+							<p>{value.collections.description}</p>
+						</div>
+					);
+				})}
+
 				<br />
 				<div className={styles.ButtonContainer}>
-					<ApplyButton text={'View All Collections'} onClick={'/'}/>
+					<ApplyButton text={'View All Collections'} onClick={'/'} />
 				</div>
 			</div>
 		</>
