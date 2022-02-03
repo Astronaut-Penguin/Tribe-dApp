d22185f962f8cd75649a846cded1a7be3a533910
FacundoRago <facundorago98@gmail.com>
2022-02-03 15:11:14 -0300

    Added a new FAQV2 Component
    This component is a new version of FAQ Section, now working on all views

---
+++ src/components/LandingV2-Components/Sections/FAQV2/Faq-Styles.js
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+export const StyledFaq = styled.div`
+  cursor: pointer;
+
+  div.faq-question {
+    margin: 0 0 -6px;
+  }
+
+  div.faq-answer {
+    overflow: hidden;
+
+    span {
+      display: block; 
+    }
+  }
+`;
+
+export const StyledFaqsList = styled.div`
+`;
+
